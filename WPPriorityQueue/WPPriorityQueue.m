8c884a7b07bfa3a193201a2882fef5833ebcfb33
chengqian <chengqian@wepie.com>
2022-09-15 01:34:24 +0800

    完善其他方法

--- WPPriorityQueue/WPPriorityQueue.m
+++ WPPriorityQueue/WPPriorityQueue.m
@@ -9,8 +9,13 @@
 
 static const NSInteger DEFAULT_CAPACITY = 10;
 
+/**
+ 数据拷贝后续使用C语言scp函数
+ */
 @interface WPPriorityQueue()
+// 记录数组存放的数据数量
 @property (nonatomic, assign) NSInteger count;
+// 比较Block
 @property (nonatomic, copy) NSComparator cmptr;
 @end
 
@@ -21,7 +26,13 @@ static const NSInteger DEFAULT_CAPACITY = 10;
 }
 
 - (void)dealloc {
-    
+    if (elements) {
+        for (NSInteger i = 0; i<=self.count; i++) {
+            // 需要做一下release吗？
+            CFRelease(elements[i]);
+        }
+        free(elements);
+    }
 }
 
 - (instancetype)initWithCapacity:(NSInteger)capacity cmptr:(NSComparator)cmptr {
@@ -39,32 +50,145 @@ static const NSInteger DEFAULT_CAPACITY = 10;
         for (NSInteger i = 0; i < array.count; i++) {
             elements[i] = (__bridge void *)(array[i]);
         }
+        [self heapify];
     }
     return self;
 }
 
+-(void)clear {
+    for (NSInteger i = 0; i < self.count; i++) {
+        elements[i] = NULL;
+    }
+    self.count = 0;
+}
+
+- (void)addElement:(id)element {
+    [self elementNotNullCheckForObj:element];
+    [self ensureCapacity:self.count + 1];
+    elements[self.count++] = (__bridge void *)(element);
+    [self siftUpForIndex:self.count - 1];
+}
+
+-(id)get {
+    [self emptyCheck];
+    return (__bridge id)(elements[0]);
+}
+
+- (id)remove {
+    [self emptyCheck];
+
+    NSInteger lastIndex = --self.count;
+    id root = (__bridge id)(elements[0]);
+    elements[0] = elements[lastIndex];
+    elements[lastIndex] = NULL;
+        
+    [self siftDownForIndex:0];
+    return root;
+}
+
+- (void)removeElement:(id)element {
+    // 在数组中查找对应的元素，
+    // 删除，之后重新排堆中的数据
+}
+
+- (id)replace:(id)element {
+    [self elementNotNullCheckForObj:element];
+    id root = NULL;
+    if (self.count == 0) {
+        elements[0] = (__bridge void *)(element);
+        self.count++;
+    } else {
+        root = (__bridge id)(elements[0]);
+        elements[0] = (__bridge void *)(element);
+        [self siftDownForIndex:0];
+    }
+    return root;
+}
+
 - (void)heapify {
-    
+    for (NSInteger i = (self.count >> 1) - 1; i >= 0; i--) {
+        [self siftDownForIndex:i];
+    }
 }
 
 - (void)siftDownForIndex:(NSInteger)index {
-    
+    id element = (__bridge id)(elements[index]);
+    NSInteger half = self.count >> 1;
+    // 第一个叶子节点的索引 == 非叶子节点的数量
+    // index < 第一个叶子节点的索引
+    // 必须保证index位置是非叶子节点
+    while (index < half) {
+        // index的节点有2种情况
+        // 1.只有左子节点
+        // 2.同时有左右子节点
+                
+        // 默认为左子节点跟它进行比较
+        NSInteger childIndex = (index << 1) + 1;
+        id child = (__bridge id)(elements[childIndex]);
+            
+        // 右子节点
+        NSInteger rightIndex = childIndex + 1;
+        
+        // 选出左右子节点最大的那个
+        if (rightIndex < self.count &&
+            self.cmptr((__bridge id)(elements[rightIndex]),child)> 0) {
+            child = (__bridge id)(elements[childIndex = rightIndex]);
+        }
+        
+        if (self.cmptr(element,child) >= 0) break;
+
+        // 将子节点存放到index位置
+        elements[index] = (__bridge void *)(child);
+        // 重新设置index
+        index = childIndex;
+    }
+    elements[index] = (__bridge void *)(element);
 }
 
 - (void)siftUpForIndex:(NSInteger)index {
+    id element = (__bridge id)(elements[index]);
+    while (index > 0) {
+        NSInteger parentIndex = (index - 1) >> 1;
+        id parent = (__bridge id)(elements[parentIndex]);
+        if (self.cmptr(element, parent) <= 0) break;
+            // 将父元素存储在index位置
+            elements[index] = (__bridge void *)(parent);
+            // 重新赋值index
+            index = parentIndex;
+    }
+    elements[index] = (__bridge void *)(element);
+}
+
+- (void)ensureCapacity:(NSInteger)capacity {
+    NSInteger oldCapacity = self.count;
+    if (oldCapacity >= capacity) return;
     
+    NSInteger newCapacity = oldCapacity + (oldCapacity >> 1);
+    void **newElements = malloc(capacity * sizeof(void *));
+    for (int i = 0; i < self.count; i++) {
+        newElements[i] = elements[i];
+    }
+    elements = newElements;
+}
+    
+- (void)emptyCheck {
+    if (self.count == 0) {
+        //TODO:这里还是要抛异常
+//        @throw [NSException exceptionWithName:@"异常名称" reason:@"异常原因" userInfo:nil]
+        NSLog(@"Heap is empty");
+    }
+}
+
+-(void)elementNotNullCheckForObj:(id)element {
+    if (!element) {
+        //TODO:这里还是要抛异常
+//        @throw [NSException exceptionWithName:@"异常名称" reason:@"异常原因" userInfo:nil]
+        NSLog(@"element must not be null");
+    }
 }
 
 - (BOOL)isEmpty {
     return self.count == 0;
-}
-
-- (NSInteger)compareObj1:(id)obj1 obj2:(id)obj2 {
-    if (self.cmptr) {
-        return self.cmptr(obj1,obj2);
-    }
-    //TODO: 这里看要怎么去实现
-    return 0;
 }
 
 @end
